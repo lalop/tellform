7ffd061f44335ef583c8162258cbdd60170c21cd
Sam <sam@lalop.co>
2016-07-27 15:54:01 +0200

    stuffs

--- public/modules/forms/base/demo/boot.js
+++ public/modules/forms/base/demo/boot.js
@@ -1,15 +1,13 @@
 
-angular.module('forms', [
-	'duScroll', 'ngResource', 'NodeForm.templates', 'pascalprecht.translate',
-	'angular-input-stars',
-	'ui.select'
-]);
-
 angular.module('NodeForm', [
     'ui.select', 'cgBusy', 'ngSanitize', 'vButton', 'ngResource',
-    'ui.router', 'ui.bootstrap', 'ui.utils', 'ngRaven', 'forms',
-	'ui.select'
+    'ui.router', 'ui.bootstrap', 'ui.utils', 'ngRaven'
 ]);
+angular.module('forms', [
+    'duScroll', 'ngResource', 'NodeForm.templates', 'pascalprecht.translate',
+    'angular-input-stars'
+]);
+angular.module('NodeForm').requires.push('forms');
 
 angular.module('forms').factory('Auth', [
   function() {
@@ -42,17 +40,20 @@ angular.module('forms').factory('$state', [function() {
 }]);
 angular.module('forms').factory('myForm', ['Forms', function(Forms) {
     var form = window.form;
-    form.visible_form_fields = _.filter(form.form_fields, function(field){
-    	return (field.deletePreserved === false);
-    });
+    angular.forEach(form.form_fields, function(field, index) {
+		if(form.form_fields[index]._id === undefined) {
+            form.form_fields[index]._id = '_'+index;
+        }
+	});
+    form.visible_form_fields = form.form_fields;
     return form;
 }]);
 angular.module('forms').constant('FORM_URL', '/form/:formId');
 
+
 angular.module('forms').value('SendVisitorData', {
 	send: function(){}
 });
-
 angular.element(document).ready(function() {
 	//Then init the app
 	angular.bootstrap(document, ['forms']);
