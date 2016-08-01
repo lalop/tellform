7370f4ad34a07fe02c43a8e9d8e895688d1dc1c0
Sam <sam@lalop.co>
2016-08-01 09:13:19 +0200

    enter shift

--- public/modules/forms/base/directives/on-enter-key.client.directive.js
+++ public/modules/forms/base/directives/on-enter-key.client.directive.js
@@ -4,15 +4,20 @@ angular.module('forms').directive('onEnterKey', ['$rootScope', function($rootSco
 	return {
 		restrict: 'A',
 		link: function($scope, $element, $attrs) {
+			var pressed = {};
 			$element.bind('keydown keypress', function(event) {
 				var keyCode = event.which || event.keyCode;
-				if(keyCode === 13) {
+				if(keyCode === 13 && !pressed[16]) {
 					$rootScope.$apply(function() {
 						$rootScope.$eval($attrs.onEnterKey);
 					});
 
 					event.preventDefault();
 				}
+				pressed[keyCode] = true;
+			}).bind('keyup', function(event) {
+				var keyCode = event.which || event.keyCode;
+				delete pressed[keyCode];
 			});
 		}
 	};
